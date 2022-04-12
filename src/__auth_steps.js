4392832dbfe51ad927eedf01b241977162792890
saifulislamhridoy <saifulislam1hridoy@gmail.com>
2022-04-12 16:02:36 +0600

    implimented login

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -3,6 +3,12 @@
 2.npm install firebase
 3.create firebase.init.js and import getAuth to export auth
 4.firebase setting > Authentication > enable Email and Password auth
+5.create login,signup component,setup route
+6.attach form field handler and form submit handler
+7.npm install --save react-firebase-hooks
+8.useCreateUserWithEmailAndPassword from react-firebase-hooks
+9.if user is created redirect to the expected the page
+10.useSignInWithEmailAndPassword for sign in
 */ 
 
 
